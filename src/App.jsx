b9f982e97447bc8a73efc0eeca8654de48bc2eaf
Rafa06060606 <84454536+Rafa06060606@users.noreply.github.com>
2026-06-17 22:07:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import CertificadoPage from './pages/Consultor/certificado.jsx';
 import DefinicoesConsultorPage from './pages/Consultor/definicoes_consultor.jsx';
 
 //admin
+import GestaoBadges from "./pages/Admin/gestao_badges.jsx";
 import GestaoContas from "./pages/Admin/gestao_contas.jsx";
 import EditarConta from "./pages/Admin/editar_contas.jsx";
 import CriarConta from "./pages/Admin/criar_conta.jsx";
@@ -90,7 +91,7 @@ function App() {
         <Route path="/admin/rgpd" element={<PoliticasRGPD />} />
         <Route path="/admin/rgpd/editar" element={<EdicaoRGPD />} />
         <Route path="/admin/notificacoes" element={<ConfigurarNotificacoes />} />
-        <Route path="/admin/badges" element={<GestaoBadges />} />
+        <Route path="/admin/badges" element={<GestaoBadges/>} />
         <Route path="/admin/badges/novo" element={<CriarBadge />} />
         <Route path="/admin/badges/editar/:id" element={<EditarBadge />} />
 
